622081cea115ecc66d44353cbcf227439b106ad7
Petrushkov_Yevhenii <petrushkov093@gmail.com>
2025-04-28 19:10:42 +0300

    fix

--- src/articles-api.js
+++ src/articles-api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
 
-export default async function fetchArticlesWithTopic({ topik }) {
+export default async function fetchArticlesWithTopic(topik) {
   const response = await axios.get(`/search?query=${topik}`);
   return response.data.hits;
 }
